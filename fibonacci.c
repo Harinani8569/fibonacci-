5afae0d99c791e7e1f1790be04df2536e21f59f6
Harinani8569 <43022328+Harinani8569@users.noreply.github.com>
2019-03-18 09:47:08 -0700

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
-#include<pthtread.h>
-void *fibo(int *n)
+#include<pthread.h>
+void *fibonacci(void* args)
 {
-}
-
-int main()
-{
-}
+  int n1=0;
+  int n2=1;
+  int fib;
+  int n,num;
+  printf("Enter the num upto which you want the series");
+  scanf("%d",num);
+  for(int couht=0;count<num;++count)
+  {
+    fib=n1+n2;
+    n1=n2;
+    n2=fib;
+  }
+  int main()
+  {
+    pthread_t t1;
+    pthread_create(&t1,NULL,&fibonacci,(void*)&n);
+    pthread_join(t1,(void *)&fib);
+    printf("The series is %d",fib);
+  }
